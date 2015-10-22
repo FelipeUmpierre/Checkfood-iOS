68f5ce857fc5c54bb30e6eb329edb51991db63c5
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-10-22 00:41:55 -0200

    Model to manager request by API to DetailsViewController

--- Checkfood/ProductDetail.swift
+++ Checkfood/ProductDetail.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+class ProductDetail {
+    var product: Product? {
+        didSet {
+            self.find()
+        }
+    }
+    
+    func find() {
+        let productDetailUrl = "\(Urls.productDetail)/\(self.product!.id)"
+        
+        Alamofire.request(.GET, productDetailUrl).responseJSON { request in
+            if let json = request.result.value {
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+                    let data = JSON(json)
+                    var ingredients: [Ingredient] = []
+                    
+                    for (_, ingredient): (String, JSON) in data["ingredients"] {
+                        ingredients += [Ingredient.init(id: ingredient["id"].int!, name: ingredient["name"].string!)]
+                    }
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.product!.ingredients = ingredients
+                        
+                        NSNotificationCenter.defaultCenter().postNotificationName(NotificationsKey.NotificationsProductDetailKey, object: nil)
+                    }
+                }
+            }
+        }
+    }
+    
+    func getProduct() -> Product {
+        return self.product!
+    }
+}
