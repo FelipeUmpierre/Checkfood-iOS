2d35d0cd94f24f23fe6296bda60b8712f9760373
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-10-27 00:57:37 -0200

    Implements NSCoding

--- Checkfood/Product.swift
+++ Checkfood/Product.swift
@@ -8,22 +8,36 @@
 
 import Foundation
 
-class Product {
+class Product: NSObject, NSCoding {
+    
+    // MARK: - Property
+    
     var id: Int
     var name: String
-    var description: String
+    var descriptive: String
     var price: Double
     
     var ingredients: [Ingredient]?
     var quantity: Int?
     var observation: String?
     
+    // MARK: - init
+    
     init(id: Int, name: String, description: String, price: Double) {
         self.id = id
         self.name = name
-        self.description = description
+        self.descriptive = description
         self.price = price
+        
+        super.init()
     }
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent(NSArchiverPath.archiverProducts)
+    
+    // MARK: - Converting Array to String
     
     func ingredientList() -> String {
         var arrayOfIngredients: [String] = [String]()
@@ -33,5 +47,38 @@ class Product {
         }
         
         return arrayOfIngredients.joinWithSeparator(", ")
+    }
+    
+    // MARK: - NSCoding
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let id = aDecoder.decodeObjectForKey("id") as! Int
+        let name = aDecoder.decodeObjectForKey("name") as! String
+        let descriptive = aDecoder.decodeObjectForKey("descriptive") as! String
+        let price = aDecoder.decodeObjectForKey("price") as! Double
+        
+        self.init(id: id, name: name, description: descriptive, price: price)
+        
+        if let ingredients = aDecoder.decodeObjectForKey("ingredients") as? [Ingredient] {
+            self.ingredients = ingredients
+        }
+        
+        if let quantity = aDecoder.decodeObjectForKey("quantity") as? Int {
+            self.quantity = quantity
+        }
+
+        if let observation = aDecoder.decodeObjectForKey("observation") as? String {
+            self.observation = observation
+        }
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.id, forKey: "id")
+        aCoder.encodeObject(self.name, forKey: "name")
+        aCoder.encodeObject(self.descriptive, forKey: "descriptive")
+        aCoder.encodeObject(self.price, forKey: "price")
+        aCoder.encodeObject(self.ingredients, forKey: "ingredients")
+        aCoder.encodeObject(self.quantity, forKey: "quantity")
+        aCoder.encodeObject(self.observation, forKey: "observation")
     }
 }
